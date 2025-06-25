d6c9cd090d0ab6c459d5d0f29b9242934c35db7b
nure-saba-binte-alam <nuresaba686@gmail.com>
2025-06-25 17:44:08 +0600

    changed user table admin to users

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Admin extends Authenticatable
 {
-    protected $table = 'admin'; // Specify your table name
+    protected $table = 'users'; // Specify your table name
 
     protected $fillable = ['email', 'password'];
 
